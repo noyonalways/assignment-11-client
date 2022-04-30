c594f86b539a3864d8fef46077c714913fa93545
codernoyon <noyonrahman2003@gmail.com>
2022-04-30 19:08:27 +0600

    singup page added

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import SocialLogin from '../../Components/SocialLogin/SocialLogin';
 
 const Register = () => {
     return (
         <div>
-            
+            <div className="container mx-auto lg:px-16 flex items-center justify-center h-screen">
+                <div class="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md border-t">
+                    <div class="px-6 py-4">
+                        <h2 class="text-3xl font-bold text-center text-gray-700 ">Register</h2>
+                        <p class="mt-1 text-center text-gray-500 ">Create an account</p>
+                        <form>
+                            <div class="w-full mt-4">
+                                <input class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="email" placeholder="Name" aria-label="Email Address" />
+                            </div>
+
+                            <div class="w-full mt-4">
+                                <input class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="email" placeholder="Email Address" aria-label="Email Address" />
+                            </div>
+
+                            <div class="w-full mt-4">
+                                <input class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password" placeholder="Password" aria-label="Password" />
+                            </div>
+
+                            <div class="w-full mt-4">
+                                <input class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password" placeholder="Confirm password" aria-label="Password" />
+                            </div>
+
+                            <div class="flex items-center justify-between mt-4">
+                                <label className='text-sm' htmlFor="terms"><input className='mr-2' type="checkbox" name="" id="terms" />Terms and condition</label>
+                                <button class="px-4 py-2 leading-5 text-white transition-colors duration-200 transform bg-blue-400 rounded hover:bg-blue-500 focus:outline-none" type="button">Register</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="flex items-center justify-center  text-center">
+                        <span class="text-sm text-gray-600 ">Already have an account? </span>
+                        <Link to='/login' class="mx-2 text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline">Login</Link>   
+                    </div>
+                    <div className="px-5 pb-4">
+                    <SocialLogin/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
