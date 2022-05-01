c8e45d6d0a9df16502e0a86c6c8c96de31da85be
codernoyon <noyonrahman2003@gmail.com>
2022-05-01 17:57:00 +0600

    more validation added on register form and header update

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageTitle from '../../Components/PageTitle/PageTitle';
 import SocialLogin from '../../Components/SocialLogin/SocialLogin';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../../Firebase/firebase.init';
 import { MdVisibility, MdVisibilityOff } from "react-icons/md";
 
@@ -14,12 +14,14 @@ const Register = () => {
     const [showPasseword, setShowPassword] = useState(false);
     const [confirmShowPasseword, setConfirmShowPassword] = useState(false);
     const [confirmPassword, setConfirmPassword] = useState({ value: '', error: '' });
+    const [terms, setTerms] = useState(false);
     const [
         createUserWithEmailAndPassword,
         user,
         loading,
         error,
     ] = useCreateUserWithEmailAndPassword(auth);
+    const [updateProfile, updating, ] = useUpdateProfile(auth);
 
 
     const handleName = inputName => {
@@ -57,11 +59,36 @@ const Register = () => {
     }
 
 
-    const handleCreateUser = event => {
+    const handleCreateUser = async (event) => {
         event.preventDefault();
+
+        if(name.value === ''){
+            setName({value: '', error: 'Name is required'})
+        }
+        if(email.value === ''){
+            setEmail({value: '', error: 'Email is required'})
+        }
+        if(password.value === ''){
+            setPassword({value: '', error: 'Password is required'})
+        }
+        if(confirmPassword.value === ''){
+            setConfirmPassword({value: '', error: 'Confirm Password is required'})
+        }
+
+        if(name.value && email.value && password.value === confirmPassword.value && terms){
+            await createUserWithEmailAndPassword(email.value, password.value);
+            await updateProfile({displayName: name.value});
+            event.target.reset();
+        }
+
+        
+
     }
 
-    console.log(email);
+    if(user){
+        console.log(user);
+    }
+
 
 
     return (
@@ -74,7 +101,7 @@ const Register = () => {
                         <p className="mt-1 text-center text-gray-500 ">Create an account</p>
                         <form onSubmit={handleCreateUser}>
                             <div className="w-full mt-4">
-                                <input onBlur={(e) => handleName(e.target.value)} className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="email" placeholder="Name" aria-label="Name" />
+                                <input onBlur={(e) => handleName(e.target.value)} className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="text" placeholder="Name" aria-label="Name" />
                                 {
                                     name?.error && (<small className='text-red-400'>{name.error}</small>)
                                 }
@@ -89,7 +116,7 @@ const Register = () => {
 
                             <div className="w-full mt-4 relative">
                                 <input onBlur={(e) => handlePassword(e.target.value)} className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type={showPasseword ? 'text' : 'password'} placeholder="Password" aria-label="Password" />
-                                <button onClick={() => setShowPassword(!showPasseword)} type='button' className='absolute top-3 right-3'>
+                                <button onClick={() => setShowPassword(!showPasseword)} type='button' className='absolute top-3 right-3 text-lg'>
                                   {showPasseword ? <MdVisibility/> : <MdVisibilityOff/> }  
                                 </button>
                                 {
@@ -99,7 +126,7 @@ const Register = () => {
 
                             <div className="w-full mt-4 relative">
                                 <input onBlur={(e) => handleConfirmPassword(e.target.value)} className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md  focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type={confirmShowPasseword ? 'text' : 'password'} placeholder="Confirm password" aria-label="Password" />
-                                <button onClick={() => setConfirmShowPassword(!confirmShowPasseword)} type='button' className='absolute top-3 right-3'>
+                                <button onClick={() => setConfirmShowPassword(!confirmShowPasseword)} type='button' className='absolute top-3 right-3 text-lg'>
                                   {confirmShowPasseword ? <MdVisibility/> : <MdVisibilityOff/> }  
                                 </button>
                                 {
@@ -108,9 +135,12 @@ const Register = () => {
                             </div>
 
                             <div className="flex items-center justify-between mt-4">
-                                <label className='text-sm' htmlFor="terms"><input className='mr-2' type="checkbox" name="" id="terms" />Terms and condition</label>
+                                <label className={`text-sm ${terms ? 'text-black' : 'text-red-400'}`} htmlFor="terms"><input onChange={(e) => setTerms(!terms)} className='mr-2' type="checkbox"  id="terms" />Terms and condition</label>
                                 <button className="px-4 py-2 leading-5 text-white transition-colors duration-200 transform bg-blue-500 rounded hover:bg-blue-400 focus:outline-none" type="submit">Register</button>
                             </div>
+                            {
+                                 error && <small className='mt-3 rounded-sm block text-red-400 bg-red-100 p-2 text-center'>{error?.message.includes('auth/email-already-in-use') ? 'User already exist' : error?.message}</small>
+                            }
                         </form>
                     </div>
                     <div className="flex items-center justify-center  text-center">
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import logo from '../../Assets/images/arredo-logo-black.png';
-import {HiOutlineX, HiMenu, HiUserCircle} from 'react-icons/hi';
+import {HiOutlineX, HiMenu, HiUserCircle, HiOutlineLogout} from 'react-icons/hi';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../Firebase/firebase.init';
+import { signOut } from 'firebase/auth';
 
 const Header = () => {
     const [open, setOpen] = useState(false);
+    const [user, loading, error] = useAuthState(auth);
     return (
-        <header className='bg-white  py-3 shadow-sm sticky top-0'>
-            <div className="container mx-auto md:px-16 px-3">
-                <nav className='flex items-center justify-between'>
+        <header className='bg-white  py-3 shadow-sm sticky top-0 z-[9999]'>
+            <div className="container mx-auto md:px-16 px-3 ">
+                <nav className='flex items-center justify-between relative'>
                     <Link className='w-28 inline-block' to='/'>
                         <img src={logo} alt="" className="w-full" />
                     </Link>
                     
-                    <ul className={`lg:flex h-screen lg:h-auto items-center lg:space-x-5 w-3/6 lg:w-auto top-0 bg-white lg:bg-transparent absolute lg:static duration-200 z-10 ${open ? 'left-0 shadow lg:shadow-none'  : "-left-[50%]" }`}>
+                    <ul className={`lg:flex h-screen lg:h-auto items-center lg:space-x-5 w-3/6 lg:w-auto -top-3 bg-white lg:bg-transparent absolute lg:static duration-200 z-10  ${open ? '-left-3 shadow lg:shadow-none'  : "-left-[55%]" }`}>
                         <li className=''>
                             <NavLink className={({isActive}) => (isActive ? "bg-black text-white lg:text-black  lg:bg-opacity-0 block px-3 py-2 lg:p-0 " : "block px-3 py-2 lg:p-0 lg:text-opacity-50 text-black hover:text-black" )} to='/'>Home</NavLink>
                         </li>
@@ -26,9 +30,13 @@ const Header = () => {
                         
                     </ul>
                     <ul className='flex'>
-                        <li>
+                        {
+                            user ? <li>
+                                <button onClick={() => signOut(auth)} className='bg-gray-200 py-2 px-3 rounded hover:bg-gray-300 flex items-center space-x-2'><HiOutlineLogout/><span>Logout</span></button>
+                            </li> : <li>
                             <NavLink to='/login'><HiUserCircle className='text-3xl'/></NavLink>
                         </li>
+                        }
                     </ul>
                     <button onClick={() => setOpen(!open)} className="lg:hidden text-3xl ">
 						{open ? <HiOutlineX /> : <HiMenu />}
